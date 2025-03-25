c9ff8076009000cad5a81341073b5a0222159c4e
Kamanashis Roy <kamanashisroy+aroopcode@gmail.com>
2025-03-25 16:35:52 -0700

    Not breaking

---
+++ BioSlime.py
@@ -0,0 +1,244 @@
+import sys
+from heapq import heappop,heappush,heapify
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+# Read in N (grid size), C (max load capacity), and H (number of harvesters)
+N = int(input())
+C = int(input())
+H = int(input())
+
+# Read the location of each harvester
+hr, hc, load = [], [], []
+for h in range(H):
+  row, col = input().split(" ")
+  hr.append( int(row) )
+  hc.append( int(col) )
+  load.append(0)
+
+grid = [[' ' for x in range(N)] for y in range(N)]
+# Read the starting grid configuration
+for r in range(N):
+  for c in range(N):
+    grid[r][c] = input()
+
+depots = []
+depotId = dict()
+for r in range(N):
+  for c in range(N):
+    if grid[r][c] == 'd':
+        depotId[(r,c)] = len(depots)
+        depots.append((r,c,grid[r][c]))
+
+# Define movement directions (right, down, left, up)
+dc = [1,0,-1,0]
+dr = [0,1,0,-1]
+dname = ['R','D','L','U']
+
+# At first find the shortest path 
+def shortestPath(begr,begc):
+    hp = [(0,begr,begc)]
+    dist = [[None]*N for _ in range(N)]
+    source = [[None]*N for _ in range(N)]
+    dist[begr][begc] = 0
+
+    while hp:
+        curdist, r, c = heappop(hp)
+        if curdist > dist[r][c]:
+            continue
+        for d in range(4):
+            nr = r + dr[d]
+            nc = c + dc[d]
+            if nc<0 or nc>=N or nr<0 or nr>=N or grid[nr][nc]=='W' or grid[nr][nc] == 'd':
+                continue
+            #eprint(r,c,'exploring ', nr,nc)
+            if dist[nr][nc] is None or dist[nr][nc] >= (curdist+1):
+                dist[nr][nc] = curdist+1
+                source[nr][nc] = []
+                heappush(hp,(curdist+1,nr,nc))
+            if dist[nr][nc] >= (curdist+1):
+                source[nr][nc].append((r,c))
+    
+    #eprint(r,c,'Done',dist)
+    return [dist,source]        
+
+
+# This path will be useful to return to depot
+spath = dict()
+for i,(r,c,tp) in enumerate(depots):
+    if 'd' == tp:
+        spath[i] = shortestPath(r,c)
+
+def calcDir(distr,distc):
+    for d in range(4):
+        if distr == dr[d] and distc == dc[d]:
+            return d
+
+# split the grid into small parts
+# total cells = N*N
+# we can make subgrid of size root(N)*root(N)
+
+RN = 1
+while (RN*RN) < N:
+    RN += 1
+
+def calcSubGridId(r,c):
+    subr,r2 = divmod(r,RN)
+    subc,c2 = divmod(c,RN)
+    return (subr,subc),(r2,c2)
+
+def shortestPathSubGrid(begr,begc,limit,explored):
+    hp = [(0,begr,begc)]
+    dist = dict()
+    source = dict()
+    dist[(begr,begc)] = 0
+
+    while hp:
+        curdist, r, c = heappop(hp)
+        if curdist > dist[(r,c)]:
+            continue
+        if grid[r][c] == 's' and begr != r and begc != c:
+
+            eprint(begr,begc,'found s',r,c)
+            stack = [(0,r,c)]
+            while stack:
+                #eprint(stack)
+                cost,rr,cc = stack.pop()
+                if dist[(rr,cc)] == 1:
+                    if (rr,cc) in explored :
+                        eprint(rr,cc,'already explored')
+                        continue
+                    eprint(begr,begc,'Should move to ',rr,cc,'to collect', r,c)
+                    return rr,cc
+                if rr == begr and cc == begc:
+                    continue
+                for pr,pc in source[(rr,cc)]:
+                    if (dist[(pr,pc)] + cost + 1) > curdist:
+                        continue
+                    stack.append( (cost+1,pr,pc))
+            eprint(begr,begc,'No steps to take!')
+            #return None,None
+        #if curdist > limit:
+        #   return None,None
+
+        for d in range(4):
+            nr = r + dr[d]
+            nc = c + dc[d]
+            if nc<0 or nc>=N or nr<0 or nr>=N or grid[nr][nc]=='W'or grid[nr][nc] == 'd':
+                continue
+            if (nr,nc) not in dist or dist[(nr,nc)] >= (curdist+1):
+                dist[(nr,nc)] = curdist+1
+                source[(nr,nc)] = []
+                heappush(hp,(curdist+1,nr,nc))
+            if dist[(nr,nc)] >= (curdist+1):
+                source[(nr,nc)].append((r,c))
+    
+    return None,None
+
+def moveToNearestDepot(r,c,explored, capacity):
+    dist = []
+    for i in range(len(depots)):
+        dist.append( (spath[i][0][r][c], i) )
+    unused,nearest = min(dist)
+
+    # now find the path to nearest depot
+    source = spath[nearest][1]
+    #eprint(source,r,c,source[r][c])
+    
+    lr,lc = source[r][c][0]
+    for lr,lc in source[r][c]:
+        if (lr,lc) in explored or (capacity >= C and grid[lr][lc] == 's'):
+            continue
+        explored.add((lr,lc))
+        return calcDir(lr-r,lc-c)
+    return None
+
+dir = [0] * H
+
+# Simulate 1000 turns
+for turn in range(0,1000):
+
+    explored = set()
+    # find the slimes and nearest harvestors
+    slimes = []
+    slimeId = dict()
+    for r in range(N):
+      for c in range(N):
+        if grid[r][c] == 's':
+            slimeId[(r,c)] = len(slimes)
+            slimes.append((r,c,grid[r][c]))
+            # return to nearest depot ?
+        elif grid[r][c] == 'W' or grid[r][c] == 'd':
+            explored.add((r,c))
+            
+    moves = [None]*H
+    for h in range(H):
+        r,c = hr[h],hc[h]
+        explored.add((r,c))
+
+    for h in range(H):
+        r,c = hr[h],hc[h]
+        if load[h]>=C:
+            # return to nearest depot ?
+            
+            moves[h] = moveToNearestDepot(r,c,explored,load[h])
+        else:
+
+            # find a slime in current subgrid
+            tgtr,tgtc = shortestPathSubGrid(r,c,N,explored)
+
+            if tgtr is not None and (tgtr,tgtc) not in explored:
+                explored.add((tgtr,tgtc))
+                moves[h] = calcDir(tgtr-r,tgtc-c)
+            else: 
+                moves[h] = moveToNearestDepot(r,c,explored,load[h])
+
+    cmd = ""
+    # Move each harvester
+    for h in range(H):
+        if moves[h] is not None:
+            d = moves[h]
+            curgrid = grid[hr[h]][:]
+            eprint(h,'cur pos', hr[h],hc[h], curgrid);
+            curgrid[hc[h]] = '!'
+            eprint(h,'cur pos', hr[h],hc[h], curgrid);
+            cmd += f"{h} {dname[d]} "
+            dir[h] = d
+            nr = hr[h] + dr[d]
+            nc = hc[h] + dc[d]
+            hr[h] = nr
+            hc[h] = nc
+            curgrid = grid[hr[h]][:]
+            eprint(d,'new pos', hr[h],hc[h], curgrid);
+            curgrid[hc[h]] = '!'
+            eprint(d,'new pos', hr[h],hc[h], curgrid);
+            explored.add((nr,nc))
+            assert(not( nc<0 or nc>=N or nr<0 or nr>=N or grid[nr][nc]=='W'))
+            #grid[hr[h]][hc[h]] = 'H'
+        else:
+            cmd += f"{h} X "
+            eprint(d,'No move', hr[h],hc[h]);
+            #grid[hr[h]][hc[h]] = 'H'
+
+    # Output the command for the turn
+    print(cmd)
+    sys.stdout.flush()
+
+    # Read the elapsed time
+    tm = int(input())
+    load = []
+
+    # Read the number of fuel carried by each harvester
+    for h in range(H):
+        load.append(int(input()))
+
+    # Read the updated grid
+    for r in range(N):
+        for c in range(N):
+            grid[r][c] = input()
+    eprint("------------------------------------------")
+    #eprint(grid)
+    for h in range(H):
+        assert('H' == grid[hr[h]][hc[h]] )
+
