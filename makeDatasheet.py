574e56e401f7b1ffb1c0b50266bca7bf81dc0994
Kamanashis Roy <kamanashisroy@gmail.com>
2025-03-31 09:18:25 -0700

    Modified scripts for capacity param tuning

--- makeDatasheet.py
+++ makeDatasheet.py
@@ -5,16 +5,17 @@ BIOSLIME='BioSlime.py'
 targets = []
 
 
-for cleanupTurn in (700,750,800,850,900):
+for cleanupTurn in (700,750,800,850,900,940):
     for N in (10,12,14,16,18,20,22,24,26,28,30):
         for D in (1,2,4,6,8,10):
             for H in (1,2,4,6,8,10,12,14,16,18,20):
-                targets.append( [f'N_{N}_D_{D}_H_{H}_{cleanupTurn}', [] ] )
-                for S in [0.1]:#(0.1,0.3,0.5):
-                    for P in [0.1,0.4]:#(0.1,0.3,0.5):
-                            for W in [0.2]:
-                                targets[-1][-1].append(f"java -jar tester.jar -exec 'python3 {BIOSLIME} -O --cleanupTurn={cleanupTurn} --noautocfg' -seed 100 -N {N} -D {D} -H {H} -delay 5 -S {S} -P {P} -W {W} -C 20 -novis >> $@")
-                                #targets[-1][-1].append(f"python3 {BIOSLIME} -A -O --noautocfg --capacityMultipliyer={capacityMultipliyer} -N {N} -D {D} -H {H} -S {S} -P {P} -W {W} -C 20 >> $@")
+                for C in (2,4,8,10):
+                    targets.append( [f'N_{N}_D_{D}_H_{H}_C_{C}_{cleanupTurn}', [] ] )
+                    for S in [0.1]:#(0.1,0.3,0.5):
+                        for P in [0.3]:#(0.1,0.3,0.5):
+                                for W in [0.2]:
+                                    targets[-1][-1].append(f"java -jar tester.jar -exec 'python3 {BIOSLIME} -O --cleanupTurn={cleanupTurn} --noautocfg' -seed 100 -N {N} -D {D} -H {H} -delay 5 -S {S} -P {P} -W {W} -C {C} -novis >> $@")
+                                    #targets[-1][-1].append(f"python3 {BIOSLIME} -A -O --noautocfg --capacityMultipliyer={capacityMultipliyer} -N {N} -D {D} -H {H} -S {S} -P {P} -W {W} -C 20 >> $@")
 
 ALL = [tgt for tgt,rules in targets]
 
--- makecsv.py
+++ makecsv.py
@@ -2,22 +2,20 @@ import os
 import re
 
 # N_15_D_3_H_8_7_13
-pat = r'..N_(?P<N>\d*)_D_(?P<D>\d*)_H_(?P<H>\d*)_(?P<cleanupTurn>\d*)'
+pat = r'..N_(?P<N>\d*)_D_(?P<D>\d*)_H_(?P<H>\d*)_C_(?P<C>\d*)_(?P<cleanupTurn>\d*)'
 p = re.compile(pat)
 table = dict()
 def parseFile(filename):
     # ./N_19_D_1_H_8_0.1.2.3.0.1.2.3
     m = p.match(filename)
-    N,D,H = int(m.group('N')),int(m.group('D')),int(m.group('H'))
-    key = (N,D,H)
+    N,D,H,C = int(m.group('N')),int(m.group('D')),int(m.group('H')),int(m.group('C'))
+    key = (N,D,H,C)
     scores = []
     print('opening',filename,key)
     with open(filename) as fd:
         for line in fd:
             if line.startswith('Score = '):
                 scores.append(float(line[8:]))
-            elif line.startswith('Score '):
-                scores.append(float(line[6:]))
     if scores:
         if key not in table:
             table[key] = []
@@ -39,23 +37,32 @@ for f in files:
 result = [None]*31
 for N in range(10,31):
     result[N] = [None]*11
+    hasNval = False
     for D in range(1,11):
+        hasDval = False
         result[N][D] = [None]*21
-        hasHval = False
         for H in range(1,21):
-            bestscore = None
-            bestparam = None
+            result[N][D][H] = [None]*11
+            hasHval = False
+            for C in range(11):
+                bestscore = None
+                bestparam = None
 
-            if (N,D,H) in table:
-                #print('collecting',table[(N,D,H)])
-                for param,scores in table[(N,D,H)]:
-                    avscore = sum(scores)/len(scores)
-                    if bestscore is None or bestscore < avscore:
-                        bestscore = avscore
-                        bestparam = param
-                hasHval = True
-                result[N][D][H] = bestparam
-        if not hasHval:
+                if (N,D,H,C) in table:
+                    #print('collecting',table[(N,D,H)])
+                    for param,scores in table[(N,D,H,C)]:
+                        avscore = sum(scores)/len(scores)
+                        if bestscore is None or bestscore < avscore:
+                            bestscore = avscore
+                            bestparam = param
+                    hasHval = True
+                    hasDval = True
+                    hasNval = True
+                    result[N][D][H][C] = bestparam
+            if not hasHval:
+                result[N][D][H] = None
+        if not hasDval:
             result[N][D] = None
-
+    if not hasNval:
+        result[N] = None
 print(result)
